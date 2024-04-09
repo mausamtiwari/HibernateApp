f72d131c8aac1c5993e186fc079fc20c751f81c7
Mausam <mausamtiwari93@gmail.com>
2024-04-09 13:52:59 +0200

    Used Lombook

--- src/main/java/be/entity/Message.java
+++ src/main/java/be/entity/Message.java
@@ -4,7 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
+
 
 @Entity
 public class Message {
@@ -24,66 +34,6 @@ public class Message {
     @Column(name = "Content")
     private String content;
 
-    public int getMessageId() {
-        return messageId;
-    }
 
-    public void setMessageId(int messageId) {
-        this.messageId = messageId;
-    }
 
-    public Integer getFromUserId() {
-        return fromUserId;
-    }
-
-    public void setFromUserId(Integer fromUserId) {
-        this.fromUserId = fromUserId;
-    }
-
-    public Integer getToUserId() {
-        return toUserId;
-    }
-
-    public void setToUserId(Integer toUserId) {
-        this.toUserId = toUserId;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Message{" +
-                "messageId=" + messageId +
-                ", fromUserId=" + fromUserId +
-                ", toUserId=" + toUserId +
-                ", subject='" + subject + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Message message)) return false;
-        return messageId == message.messageId && Objects.equals(fromUserId, message.fromUserId) && Objects.equals(toUserId, message.toUserId) && Objects.equals(subject, message.subject) && Objects.equals(content, message.content);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(messageId, fromUserId, toUserId, subject, content);
-    }
 }
--- src/main/java/be/entity/User.java
+++ src/main/java/be/entity/User.java
@@ -2,7 +2,15 @@ package be.entity;
 
 import jakarta.persistence.*;
 
-import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 
 @Entity
 public class User {
@@ -11,67 +19,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
 
-    @Column(name= "FirstName")
+    @Column(name = "FirstName")
     private String firstName;
 
-    @Column(name ="LastName")
+    @Column(name = "LastName")
     private String lastName;
 
-    @Column(name ="Address")
+    @Column(name = "Address")
     private String address;
 
     @Column(name = "City")
     private String city;
 
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User user)) return false;
-        return userId == user.userId && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(address, user.address) && Objects.equals(city, user.city);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userId, firstName, lastName, address, city);
-    }
 }
